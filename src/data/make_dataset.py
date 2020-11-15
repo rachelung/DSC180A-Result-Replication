f440b7a23075a11c23999edbc93c070ec53f4525
rachelung <42155462+rachelung@users.noreply.github.com>
2020-11-15 15:59:02 -0800

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -53,4 +53,22 @@ def get_data(outdir):
 
     #return read_train(outdir)
     
+ def read_train(datadir):
+    '''
+    Reads raw training data from disk.
+    (Would normally be more complicated!)
+    '''
+    fp = os.path.join(datadir, 'train.csv')
+    return pd.read_csv(fp)
+
+
+def read_test(datadir):
+    '''
+    Reads raw test data from disk.
+    (Would normally be more complicated!)
+    '''
+
+    fp = os.path.join(datadir, 'test.csv')
+    return pd.read_csv(fp)
+    
   
