e39c94be03481864d7c5ac15cd9bfab0e21d4a17
rachelung <42155462+rachelung@users.noreply.github.com>
2020-11-15 16:22:59 -0800

    Update build_features.py

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,11 +4,10 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-def preprocess(text_file):
+def preprocess(df):
   """
   takes in a text_file to reformat file into a pandas dataframe
   """
-  df = pd.read_csv(text_file, header=None, sep='\n') # reads each line in as its own document
   df[0] = df[0].apply(lambda w: w.lower()) # makes all words lowercase
   df['listed'] = df[0].apply(lambda x: x.split(" ")) # changes representation from string -> list of strings
   df['num_words'] = df['listed'].apply(len) # words per document
